ed436688a788d3dc058af7115d1546baa713bc01
haydencj <haydenjohns02@gmail.com>
2025-04-11 23:00:23 -0500

    Initial Commit

---
+++ PennyWise/PennyWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyWiseApp.swift
+//  PennyWise
+//
+//  Created by Hayden Johnson on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PennyWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PennyWiseTests/PennyWiseTests.swift
@@ -0,0 +1,16 @@
+//
+//  PennyWiseTests.swift
+//  PennyWiseTests
+//
+//  Created by Hayden Johnson on 4/11/25.
+//
+
+import Testing
+
+struct PennyWiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
