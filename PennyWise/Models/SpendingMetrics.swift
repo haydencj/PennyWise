cb787ac0020596e2cc76f11cd547e91e24ad69e9
haydencj <haydenjohns02@gmail.com>
2025-04-11 23:20:10 -0500

    created spending metrics data model

---
+++ PennyWise/Models/SpendingMetrics.swift
@@ -0,0 +1,20 @@
+//
+//  SpendingMetrics.swift
+//  PennyWise
+//
+//  Created by Hayden Johnson on 4/11/25.
+//
+
+import Foundation
+
+// This struct defines the basic spending metrics for the app.
+struct SpendingMetrics {
+    var spentToday: Double
+    var spentThisWeek: Double
+    var spentThisMonth: Double
+
+    // Optional: Uncomment or add these later when needed.
+    // var spentYesterday: Double?
+    // var spentLastWeek: Double?
+    // var spentLastMonth: Double?
+}
